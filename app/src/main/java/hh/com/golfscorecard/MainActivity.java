8718be371dbd23ffa30594cd5aa5816dd6e953b2
HAN HWI <ming037@naver.com>
2017-02-16 01:32:24 +0900

    Add SharedPreferences to save scores

--- app/src/main/java/hh/com/golfscorecard/MainActivity.java
+++ app/src/main/java/hh/com/golfscorecard/MainActivity.java
@@ -1,27 +1,50 @@
 package hh.com.golfscorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
 
 
+    private static final String PREFS_FILE = "hh.com.golfscorecard.preferences";
+    private static final String KEY_STROKEDCOUNT = "key_strokecount" ;
     private Hole[] mHoles = new Hole[18];
     private ListAdapter mListAdapter;
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialize SharedPreferences
+        mSharedPreferences =  getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+
         //Initialize holes
         int strokes = 0;
         for(int i=0 ; i< mHoles.length ;i++) {
-            mHoles[i] = new Hole("Hole" + (i + 1) + " :", 0);
+            strokes = mSharedPreferences.getInt(KEY_STROKEDCOUNT+i, 0);
+            mHoles[i] = new Hole("Hole" + (i + 1) + " :", strokes);
         }
 
         mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for(int i=0; i<mHoles.length ;i++)
+        {
+            mEditor.putInt(KEY_STROKEDCOUNT+i, mHoles[i].getStrokeCount());
+        }
+        mEditor.apply();
     }
 }
